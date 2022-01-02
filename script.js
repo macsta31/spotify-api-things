9f58609c137c84b621e0c03644b40c93f62f1029
macsta31 <mackstathis@gmail.com>
2022-01-02 18:48:16 -0500

    finished functionallity
    couple visual bugs at small screen size

--- script.js
+++ script.js
@@ -21,9 +21,9 @@ const APIController = (function() {
     }
 
     const _getTracks = async (token, songTitle) => {
-        console.log(songTitle);
 
-        const result = await fetch(`https://api.spotify.com/v1/search?q=track%3A${songTitle}&type=track&market=ES&limit=10&offset=5`, {
+
+        const result = await fetch(`https://api.spotify.com/v1/search?q=track%3A${songTitle}&type=track&market=US&limit=15&offset=0`, {
             method: 'GET',
             headers: {
                 'Authorization' : 'Bearer ' + token
@@ -33,13 +33,28 @@ const APIController = (function() {
         return data.tracks.items;
     }
 
+    const _getTrack = async(token, id) => {
+        const result = await fetch(`https://api.spotify.com/v1/tracks/${id}`, {
+            method: 'GET',
+            headers: {
+                'Authorization' : 'Bearer ' + token
+            }
+        });
+        const data = await result.json();
+        return data;
+    }
+
     return {
         getToken() {
             return _getToken();
         },
-        _getTracks(token, songTitle) {
+        getTracks(token, songTitle) {
             return _getTracks(token, songTitle);
+        },
+        getTrack(token, id){
+            return _getTrack(token, id);
         }
+
     }
 
 })();
@@ -53,10 +68,12 @@ const UIController = (function() {
 
         songs: '#songs',
         song : '#search-bar',
+        data: '#lyrics',
         submitButton : '#button',
-        dataBox : '#data-box',
         hfToken: '#hidden_token',
-        hfSong: '#hidden_song_title'
+        hfSong: '#hidden_song_title',
+        listItem: '.list-item',
+        songButton: '.song-button'
     }
 
 
@@ -67,12 +84,45 @@ const UIController = (function() {
                 songTitle: document.querySelector(DOMElements.song),
                 songs: document.querySelector(DOMElements.songs),
                 button: document.querySelector(DOMElements.submitButton),
-                data: document.querySelector(DOMElements.dataBox),
+                data: document.querySelector(DOMElements.data),
+                listItem: document.querySelector(DOMElements.listItem),
+                songButton: document.querySelector(DOMElements.songButton)
             }
         },
-        createTrack(name, artist) {
-            const html = `<a href="#" class="list-item">${name} <br> By: ${artist}</a>`;
+        createTrack(name, artist, img, id) {
+            const html = `
+            <button class="song-button" value="${id}">
+                            <p style="display:none">${id}</p>
+                            <a href="#">${name} <br> By: ${artist}</a>
+                            <img src="${img}" class="list-item-image" value="${id}">
+
+                        </button>`;
+            
             document.querySelector(DOMElements.songs).insertAdjacentHTML('beforeend', html);
+
+        },
+
+        createSongData(name, artist, img, duration, popularity, date, explicit){
+
+            html = `
+    
+                <div id="img-box">
+                    <img class='data-image' src="${img}" alt="">
+                </div>
+                <div id="song-metaData">
+                    <h2>Artist: ${artist}</h2>
+                    <h2>Name: ${name}</h2>
+                    </h2>
+                    <p>Release Date: ${date}</p>
+                    <p>Duration: ${duration} </p>
+                    <p>Explicit: ${explicit} </p>
+                    <p>Popularity: ${popularity} </p>
+                    
+                </div>`;
+
+
+            document.querySelector(DOMElements.data).insertAdjacentHTML('beforeend', html);
+
         },
 
         storeToken(value) {
@@ -92,10 +142,20 @@ const UIController = (function() {
                 token: document.querySelector(DOMElements.hfSong).value
             }
         },
+        getSongId() {
+            return {
+                id: document.querySelector(DOMElements.listItem).value
+            }
+        },
 
         resetSongs() {
             this.inputField().songs.innerHTML = '';
+        },
+
+        resetSongData() {
+            this.inputField().data.innerHTML = '';
         }
+
 
     }
 })();
@@ -115,19 +175,45 @@ const APPController = (function(UICtrl, APICtrl) {
         const token = await APICtrl.getToken();
 
         const songTitle =  await DOMInputs.songTitle.value;
-        console.log(songTitle);
+
         
 
         
         UICtrl.storeToken(token);
         
 
-        const songs = await APICtrl._getTracks(token, songTitle);
+        const songs = await APICtrl.getTracks(token, songTitle);
 
 
         songs.forEach(s => {
-            UICtrl.createTrack(s.name, s.artists[0].name);
+            UICtrl.createTrack(s.name, s.artists[0].name, s.album.images[1].url, s.id);
         });
+
+    });
+
+    const getDuration = (duration) => {
+        var minutes = Math.floor(duration / 60000);
+        var seconds = ((duration % 60000) / 1000).toFixed(0);
+        return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
+    }
+
+    DOMInputs.songs.addEventListener('click', async (e) => {
+        e.preventDefault();
+
+        UICtrl.resetSongData();
+
+        const token = await APICtrl.getToken();
+
+        const id = e.path[1].childNodes[1].innerText
+
+        const data = await APICtrl.getTrack(token, id);
+
+        const duration = getDuration(data.duration_ms);
+
+
+
+        UICtrl.createSongData(data.name, data.artists[0].name, data.album.images[0].url, duration, data.popularity, data.album.release_date, data.explicit);
+
 
     });
 
